c5224586d39ae1e7cf8903eea461cd1ecd3769e5
HarshaAkasam <119839653+HarshaAkasam@users.noreply.github.com>
2023-08-11 10:38:07 +0530

    import java.util.Scanner; public class imput{     public static void main(String[] args){         Scanner read=new Scanner(System.in);         String s;         s=read.nextLine();         System.out.print(s);     } }

---
+++ Take_Input_And_Print.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class imput{
+    public static void main(String[] args){
+        Scanner read=new Scanner(System.in);
+        String s;
+        s=read.nextLine();
+        System.out.print(s);
+    }
+}
