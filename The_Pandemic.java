9342349cecb23ae38750ae78bfe2cca4a7609432
HarshaAkasam <119839653+HarshaAkasam@users.noreply.github.com>
2023-08-12 11:46:11 +0530

    import java.util.*; public class main{     public static void main(String[] args){         Scanner read=new Scanner(System.in);         int N=read.nextInt();         if(N==1){             System.out.printf("0");         }         else if(N==2){             System.out.printf("1");         }         else{             System.out.printf("2");         }
        }
    }

---
+++ The_Pandemic.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class main{
+    public static void main(String[] args){
+        Scanner read=new Scanner(System.in);
+        int N=read.nextInt();
+        if(N==1){
+            System.out.printf("0");
+        }
+        else if(N==2){
+            System.out.printf("1");
+        }
+        else{
+            System.out.printf("2");
+        }
+        
+    }
+}
