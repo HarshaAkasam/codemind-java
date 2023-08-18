c062375050ff0a2217fa105c038a6532869c8ff2
HarshaAkasam <119839653+HarshaAkasam@users.noreply.github.com>
2023-08-18 11:23:40 +0530

    import java.util.Scanner; public class Print{     public static void main(String [] args){         Scanner read = new Scanner(System.in);         int n = read.nextInt();         int i,j;         for(i=1;i<=n;i++)         {             for(j=n;j>=1;j--)             System.out.print(j+" ");             System.out.print(" ");         }     } }

---
+++ print_the_following_pattern_6.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Print{
+    public static void main(String [] args){
+        Scanner read = new Scanner(System.in);
+        int n = read.nextInt();
+        int i,j;
+        for(i=1;i<=n;i++)
+        {
+            for(j=n;j>=1;j--)
+            System.out.print(j+" ");
+            System.out.print("
+");
+        }
+    }
+}
